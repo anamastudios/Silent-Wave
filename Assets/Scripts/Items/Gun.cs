330eb634473f32f002d03730ee3902b1d081c4c3
anamastudios <mr.tentakel94@gmail.com>
2025-11-11 22:07:17 +0100

    checkpoint 10

--- Assets/Scripts/Items/Gun.cs
+++ Assets/Scripts/Items/Gun.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public GameObject firepoint;
+    public Transform firepoint;
     public GameObject bulletPrefab;
     public AudioSource shootAudio;
 
+    Rigidbody2D bulletRB;
     int magazineSize = 7;
     float fireRate;
     float nextFire;
 
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(firepoint.right * 25f, ForceMode2D.Impulse);
+
+        shootAudio.Play();
     }
 }
