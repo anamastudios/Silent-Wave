a35503f565e98d8077a18ddfe79229da89aed2cc
anamastudios <mr.tentakel94@gmail.com>
2025-11-26 15:03:15 +0100

    checkpoint 19.1

--- Assets/Scripts/Mutant.cs
+++ Assets/Scripts/Mutant.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class Mutant : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rbMutant;
+    public SpriteRenderer attackSpriteChange;
+    public Sprite normalSprite;
+    public Sprite attackSprite;
+    public GameObject Model;
+    public BoxCollider2D collision;
+    public CircleCollider2D attackrange;
+    public float attackTime;
+    public float attackDamage;
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
+    bool allowMovement = false;
+    Transform transPlayer;
+
+    void Awake()
     {
-        
+        transPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 playerPos = (transPlayer.position - rbMutant.transform.position).normalized;
+
+        if (Model.activeSelf)
+            allowMovement = true;
+
+        if (allowMovement)
+        {
+            collision.enabled = true;
+            attackrange.enabled = true;
+            rbMutant.MovePosition(rbMutant.transform.position + playerPos * speed * Time.fixedDeltaTime);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            allowMovement = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            allowMovement = true;
+        }
     }
 }
